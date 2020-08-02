0875c0eff31554e69124daf20e2e1708d83b2caf
Hyunwoo <hyunwoo.k9120@gmail.com>
2020-08-03 00:53:51 +0900

    Set image and Add diary list Change the code of dropdown button; weather and emotion

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_icons/weather_icons.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -21,12 +22,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// 메인
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+
+  Widget _buildDiaryList(Diary diary) {
+    return ListTile(
+      leading: Container(
+        width: 100,
+        child: diary.image,
+      ),
+      title: Text(diary.date + '    ' + diary.title),
+      subtitle: Text(diary.content),
+      trailing: Icon(Icons.more_vert),
+      isThreeLine: true,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,39 +58,50 @@ class _MainPageState extends State<MainPage> {
               ),
             ),
           ),
-          SliverFillRemaining(
-            child: Center(
-              child: Text('diary list'),
-            ),
-          )
+          SliverList(
+              delegate: SliverChildListDelegate(_diaryList.map((e) => _buildDiaryList(e)).toList()),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.create),
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async{
+          await Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => WriteDiary()),
+            MaterialPageRoute(builder: (context) => WriteDiaryPage()),
           );
+          setState(() {
+
+          });
         },
       ),
     );
   }
 }
 
-class WriteDiary extends StatefulWidget {
+/// 다이어리 작성
+class WriteDiaryPage extends StatefulWidget {
   @override
-  _WriteDiaryState createState() => _WriteDiaryState();
+  _WriteDiaryPageState createState() => _WriteDiaryPageState();
 }
 
-class _WriteDiaryState extends State<WriteDiary> {
-  DateTime _date = DateTime.now();
+class _WriteDiaryPageState extends State<WriteDiaryPage> {
+  Weather selectedWeather;
+  Emotion selectedEmotion;
 
-  String _weather;
-  String _emotion;
+  DateTime _date = DateTime.now();
 
   PickedFile _image;
   final picker = ImagePicker();
+
+  var _titleController = TextEditingController();
+  var _contentController = TextEditingController();
+
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
 
   setDate(DateTime date) {
     var dateParse = DateTime.parse(date.toString());
@@ -84,165 +111,90 @@ class _WriteDiaryState extends State<WriteDiary> {
     return formattedDate;
   }
 
-  Widget setWeather() {
-    DropdownButton _itemDown() => DropdownButton<String>(
-          hint: Text('날씨'),
-          value: _weather,
-          isExpanded: true,
-          onChanged: (String newWeather) {
-            setState(() {
-              _weather = newWeather;
-            });
-          },
-          items: [
-            DropdownMenuItem(
-              value: "1",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8.0),
-                    child: Icon(WeatherIcons.day_sunny),
-                  ),
-                  Text(
-                    "맑음",
-                  ),
-                ],
-              ),
-            ),
-            DropdownMenuItem(
-              value: "2",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8.0),
-                    child: Icon(WeatherIcons.cloudy),
-                  ),
-                  Text(
-                    "흐림",
-                  ),
-                ],
-              ),
-            ),
-            DropdownMenuItem(
-              value: "3",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8.0),
-                    child: Icon(WeatherIcons.rain),
-                  ),
-                  Text(
-                    "비",
-                  ),
-                ],
-              ),
-            ),
-            DropdownMenuItem(
-              value: "4",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8.0),
-                    child: Icon(WeatherIcons.snow),
-                  ),
-                  Text(
-                    "눈",
-                  ),
-                ],
-              ),
-            ),
-          ],
-        );
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
 
-    return _itemDown();
+    setState(() {
+      _image = PickedFile(pickedFile.path);
+    });
+  }
+
+  Widget setWeather() {
+    return DropdownButton<Weather>(
+      hint: Text('날씨'),
+      isExpanded: true,
+      value: selectedWeather,
+      onChanged: (Weather value) {
+        setState(() {
+          selectedWeather = value;
+        });
+      },
+      items: _weatherList.map((Weather weather) {
+        return DropdownMenuItem<Weather>(
+            value: weather,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: weather.iconWeather,
+                ),
+                SizedBox(width: 1),
+                Text(weather.strWeather)
+              ],
+            ));
+      }).toList(),
+    );
   }
 
   Widget setEmotion() {
-    DropdownButton _itemDown() => DropdownButton<String>(
-          hint: Text('기분'),
-          value: _emotion,
-          isExpanded: true,
-          onChanged: (String newEmotion) {
-            setState(() {
-              _emotion = newEmotion;
-            });
-          },
-          items: [
-            DropdownMenuItem(
-              value: "0",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.sentiment_very_satisfied),
-                  Text(
-                    "행복",
-                  ),
-                ],
-              ),
-            ),
-            DropdownMenuItem(
-              value: "1",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.sentiment_satisfied),
-                  Text(
-                    "좋음",
-                  ),
-                ],
-              ),
-            ),
-            DropdownMenuItem(
-              value: "2",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.sentiment_neutral),
-                  Text(
-                    "그냥",
-                  ),
-                ],
-              ),
-            ),
-            DropdownMenuItem(
-              value: "3",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.sentiment_dissatisfied),
-                  Text(
-                    "나쁨",
-                  ),
-                ],
-              ),
-            ),
-            DropdownMenuItem(
-              value: "4",
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.sentiment_very_dissatisfied),
-                  Text(
-                    "최악",
-                  ),
-                ],
-              ),
-            ),
-          ],
-        );
-
-    return _itemDown();
+    return DropdownButton<Emotion>(
+      hint: Text('기분'),
+      isExpanded: true,
+      value: selectedEmotion,
+      onChanged: (Emotion value) {
+        setState(() {
+          selectedEmotion = value;
+        });
+      },
+      items: _emotionList.map((Emotion emotion) {
+        return DropdownMenuItem<Emotion>(
+            value: emotion,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[emotion.iconEmotion, Text(emotion.strEmotion)],
+            ));
+      }).toList(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('write'),
+        title: Text('Write'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Done',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {
+              _addDiary(Diary(
+                  setDate(_date),
+                  selectedWeather,
+                  selectedEmotion,
+                  _titleController.text,
+                  Image.file(File(_image.path)),
+                  _contentController.text));
+
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
       body: ListView(
         children: <Widget>[
@@ -284,6 +236,7 @@ class _WriteDiaryState extends State<WriteDiary> {
           Padding(
             padding: EdgeInsets.fromLTRB(20, 0, 20, 8),
             child: TextField(
+              controller: _titleController,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               decoration: InputDecoration(
                   isDense: true,
@@ -294,21 +247,36 @@ class _WriteDiaryState extends State<WriteDiary> {
           Padding(
             padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
             child: Container(
-              alignment: Alignment.bottomRight,
-              decoration: myBoxDecoration(),
-              height: 200,
-              child: IconButton(
-                icon: Icon(
-                  Icons.add_photo_alternate,
-                  size: 40,
-                ),
-                onPressed: getImage,
-              ),
-            ),
+                decoration: myBoxDecoration(),
+                height: 250,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      child: _image == null
+                          ? Text('No image')
+                          : Image.file(
+                              File(_image.path),
+                              alignment: Alignment.center,
+                            ),
+                    ),
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.add_photo_alternate,
+                          size: 40,
+                        ),
+                        onPressed: getImage,
+                      ),
+                    )
+                  ],
+                )),
           ),
           Padding(
             padding: EdgeInsets.fromLTRB(20, 10, 20, 8),
             child: TextField(
+              controller: _contentController,
               maxLength: 500,
               maxLines: 25,
               style: TextStyle(fontSize: 15),
@@ -332,11 +300,52 @@ class _WriteDiaryState extends State<WriteDiary> {
         borderRadius: BorderRadius.all(Radius.circular(10)));
   }
 
-  Future getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-    setState(() {
-      _image = PickedFile(pickedFile.path);
-    });
+  void _addDiary(Diary diary) {
+    _diaryList.add(diary);
   }
 }
+
+/// 날씨 클래스
+class Weather {
+  String strWeather;
+  Icon iconWeather;
+
+  Weather(this.strWeather, this.iconWeather);
+}
+
+/// 기분 클래스
+class Emotion {
+  String strEmotion;
+  Icon iconEmotion;
+
+  Emotion(this.strEmotion, this.iconEmotion);
+}
+
+/// 다이어리 클래스
+class Diary {
+  String date;
+  Weather weather;
+  Emotion emotion;
+  String title;
+  Image image;
+  String content;
+
+  Diary(this.date, this.weather, this.emotion, this.title, this.image,
+      this.content);
+}
+
+// 다이어리 목록을 저장할 리스트
+final _diaryList = <Diary>[];
+final _weatherList = <Weather>[
+  Weather('맑음', Icon(WeatherIcons.day_sunny)),
+  Weather('흐림', Icon(WeatherIcons.cloudy)),
+  Weather('비', Icon(WeatherIcons.rain)),
+  Weather('눈', Icon(WeatherIcons.snow))
+];
+final _emotionList = <Emotion>[
+  Emotion('행복', Icon(Icons.sentiment_very_satisfied)),
+  Emotion('좋음', Icon(Icons.sentiment_satisfied)),
+  Emotion('그냥', Icon(Icons.sentiment_neutral)),
+  Emotion('나쁨', Icon(Icons.sentiment_dissatisfied)),
+  Emotion('최악', Icon(Icons.sentiment_very_dissatisfied)),
+];
